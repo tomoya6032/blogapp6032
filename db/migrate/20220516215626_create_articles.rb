b959f4ed270a6d003541d0217eb359cde7f5548c
篠原智哉 <tomoya6032@yahoo.co.jp>
2022-05-19 05:12:25 +0900

    Articleモデルを作成した

--- db/migrate/20220516215626_create_articles.rb
+++ db/migrate/20220516215626_create_articles.rb
@@ -2,7 +2,7 @@ class CreateArticles < ActiveRecord::Migration[6.0]
   def change
     create_table :articles do |t|
       t.string :title
-      t.text :content
+      
       t.timestamps
     end
   end
