3240eeebb2f74562ecd6b8ab5770c2c4c8ffb71f
篠原智哉 <tomoya6032@yahoo.co.jp>
2022-06-08 10:58:17 +0900

    戻るボタンまでのの実装をした

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
    root to: 'articles#index'
   # get '/' => 'home#index'
   # get '/about' => 'home#about'
-  resources :articles,only: [:show, :new, :create, :edit, :update, :destroy]
+  resources :articles  #,only: [:show, :new, :create, :edit, :update, :destroy]
 end
