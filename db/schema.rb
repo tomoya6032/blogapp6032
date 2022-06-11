bdf0102400ebbfd1f21ba8994f60bfa1e95934b9
篠原智哉 <tomoya6032@yahoo.co.jp>
2022-06-11 14:23:06 +0900

    一難去ってまた一難。 表示エラーが見つからない

--- db/schema.rb
+++ db/schema.rb
@@ -13,11 +13,11 @@
 ActiveRecord::Schema.define(version: 2022_06_11_010232) do
 
   create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
+    t.integer "user_id", null: false
+    t.string "title", null: false
+    t.text "content", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
