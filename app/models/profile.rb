0c81cb1e84baaa7af993dc9180c45d165588d331
篠原智哉 <tomoya6032@yahoo.co.jp>
2022-06-17 06:26:52 +0900

    自己紹介ページで年齢と性別が日本語対応できた

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -17,6 +17,18 @@
 #  index_profiles_on_user_id  (user_id)
 #
 class Profile < ApplicationRecord
-    enum gender: {male: 0, female: 1, other: 2 }
-    belongs_to :user
+  enum gender: {male: 0, female: 1, other: 2 }
+  belongs_to :user
+
+  def age
+    return '不明' unless birthday.present?
+    years = Time.zone.now.year - birthday.year
+    days = Time.zone.now.yday - birthday.yday
+
+    if days < 0
+      "#{years -1}歳"
+    else
+      "#{years}歳"
+    end
+  end
 end
