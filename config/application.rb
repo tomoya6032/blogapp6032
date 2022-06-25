6ea275e1ee2dfd85a3b00f91540207897ffa05a2
篠原智哉 <tomoya6032@yahoo.co.jp>
2022-06-26 07:01:28 +0900

    no message

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,6 @@ module BlogApp
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.i18n.default_locale = :ja
+    config.assets.initialize_on_precompile = false 
   end
 end
