3eb8395c28b7f63404609c42783b8ffaeaf6bf92
篠原智哉 <tomoya6032@yahoo.co.jp>
2022-06-29 07:04:44 +0900

    no message

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,17 +8,15 @@
 
 jon = User.create!(email: 'john@example.com', password: 'password')
 emily = User.create!(email: 'emily@example.com', password: 'password')
-
 5.times do
   jon.articles.create!(
     title: Faker::Lorem.sentence(word_count: 5),
     content: Faker::Lorem.sentence(word_count: 100)
   )
 end
-
 5.times do
   emily.articles.create!(
     title: Faker::Lorem.sentence(word_count: 5),
-    content: Faker::Lorem.sentence(word_count: 100) 
+    content: Faker::Lorem.sentence(word_count: 100)
   )
 end
